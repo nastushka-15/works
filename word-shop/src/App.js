822b6eaac19b4f8d1a510b01ece7617673dec9c9
nastushka-15 <nastushka-15@yandex.ru>
2024-09-26 23:35:02 +0300

    week23

--- word-shop/src/App.js
+++ word-shop/src/App.js
@@ -1,23 +1,16 @@
-
 import './App.css';
+import './mainStyle.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import words from './words';
-import Card from './components/Cards/Cards';
+import WordList from './components/WordList/WordList';
+
 function App() {
   return (
     <div className="App">
       <Header/>
       <main className='card-container'>
-      {words.map((card) =>(
-        <Card
-        key={card.id}
-        english={card.english}
-        transcription={card.transcription}
-        russian={card.russian}
-        />
-      ))}
-    </main>
+        <WordList/>
+      </main>
       <Footer/>
     </div>
   );
---
+++ word-shop/src/components/WordList/WordList.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import  data from './data.json';
+import   './WordList.css';
+const WordList = ({words}) => {
+const [editIndex, setEditIndex] = useState(-1);
+
+const handleEdit = (index) => {
+setEditIndex(index);
+};
+
+const handleSave = (index) => {
+// реализация сохранения изменений
+
+setEditIndex(-1);
+};
+
+const handleCancel = () => {
+setEditIndex(-1);
+};
+
+return (
+<table>
+    <thead>
+    <tr className='word-row'>
+        <th>ID</th>
+        <th>English</th>
+        <th>Transcription</th>
+        <th>Russian</th>
+        <th>Actions</th>
+    </tr>
+    </thead>
+
+    <tbody className='row-Container'>
+    {data.map((word, index) => (
+        <tr key={word.id} className="word-row">
+        <td>{word.id}</td>
+        {editIndex === index ? (
+            <>
+            <td><input type="text" className='input' defaultValue={word.english} /></td>
+            <td><input type="text" className='input' defaultValue={word.transcription} /></td>
+            <td><input type="text" className='input' defaultValue={word.russian} /></td>
+            <td>
+                <button className='btn btn_save' onClick={() => handleSave(index)}>Save</button>
+                <button className='btn btn_cancel' onClick={handleCancel}>Cancel</button>
+            </td>
+            </>
+        ) : (
+            <>
+            <td>{word.english}</td>
+            <td>{word.transcription}</td>
+            <td>{word.russian}</td>
+            <td>
+                <button className='btn btn_edit' onClick={() => handleEdit(index)}>Edit</button>
+                <button className='btn btn_delete' >Delete</button>
+            </td>
+            </>
+        )}
+        </tr>
+    ))}
+    </tbody>
+</table>
+);
+};
+
+export default WordList;
