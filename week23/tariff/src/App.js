63fca5987d63f4cc0ec649e67967df07f4c06eb6
nastushka-15 <nastushka-15@yandex.ru>
2024-10-02 02:48:24 +0300

    дз 24 тариф

--- week23/tariff/src/App.js
+++ week23/tariff/src/App.js
@@ -1,13 +1,19 @@
+
 import './App.css';
 import Card from './components/Card/Card';
-
+import data from './data.json';
 function App() {
   return (
     <div className="App">
-      <Card price={300} speed={10} color={'blue'}/>
-      <Card price={450} speed={50} color={'green'}/>
-      <Card price={550} speed={100} color={'red'}isHighlighted={true} />
-      <Card price={1000}speed={200} color={'black'}/>
+     {data.map((card, index) => (
+<Card
+key={index}
+price={card.price}
+speed={card.speed}
+color={card.color}
+isHighlighted={card.isHighlighted}
+/>
+))}
     </div>
   );
 }
--- week23/tariff/src/components/Card/Card.jsx
+++ week23/tariff/src/components/Card/Card.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, {useState} from 'react';
 import '../Card/Card.css';
 
 function Card (props) {
-const {price, color, speed, isHighlighted}=props;
+const {price, color, speed}=props;
+const [clicked, setClicked] = useState(props.clicked || false);
+const handleChange = () =>{
+    setClicked(!clicked)
+}
 return (
-    <div className={`card ${isHighlighted ? "highlighted" : ""}`}>
+    <div className={`card ${clicked ? "highlighted" : ""}`} onClick={handleChange}>
         <div className={color}>
             <h4 className="card_header">Безлимитный {price}</h4>
             <h2 className="big">{price} <span className="normal">руб/мес</span></h2>
